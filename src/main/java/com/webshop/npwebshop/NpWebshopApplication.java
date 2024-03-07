abbd9867df3578efb4fb9c351bae5410e9c54749
Piotr Falkowski <piotrfalkowski721@gmail.com>
2024-03-07 22:26:06 +0100

    init

---
+++ src/main/java/com/webshop/npwebshop/NpWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.npwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshop/npwebshop/NpWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.npwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
